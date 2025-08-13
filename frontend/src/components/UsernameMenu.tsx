83c5ab54b026c520501e48111569f1a1bbd907a9
OmarEiissa <eissa0383@gmail.com>
2025-08-13 05:20:21 +0300

    fix: update logout function to include returnTo parameter for better redirection

--- frontend/src/components/UsernameMenu.tsx
+++ frontend/src/components/UsernameMenu.tsx
@@ -38,7 +38,9 @@ const UsernameMenu = () => {
 
           <DropdownMenuItem>
             <Button
-              onClick={() => logout()}
+              onClick={() =>
+                logout({ logoutParams: { returnTo: window.location.origin } })
+              }
               className="flex flex-1 font-bold bg-orange-500 cursor-pointer"
             >
               Log Out
