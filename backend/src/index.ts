4206c4a5ac5265ff6be1ea6462a63473c356e4ec
OmarEiissa <eissa0383@gmail.com>
2025-08-13 05:43:13 +0300

    test return last change

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,12 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.get("/health", async (req: Request, res: Response) => {
   res.send("Server is healthy! ✅");
